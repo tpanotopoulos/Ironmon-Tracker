b60e6c7ea9fcea7131f9aefb80dce07aac7cc462
tpanotopoulos <60086212+tpanotopoulos@users.noreply.github.com>
2022-09-28 12:42:31 -0700

    implementation finished

--- ironmon_tracker/CoverageCheck.lua
+++ ironmon_tracker/CoverageCheck.lua
@@ -4,8 +4,8 @@ Coverage = {
 }
 
 function Coverage.update(pokemon)
-        general = 2
-        shedinja = 0
+        Coverage.general = 2
+        Coverage.shedinja = 0
 	
 	local shedType = {PokemonData.Types.BUG, PokemonData.Types.GHOST}
         
@@ -30,56 +30,87 @@ function Coverage.update(pokemon)
 		
 		if effective = 0 then
 			--found type combo immune to full moveset, ends early
-			general = 0
+			Coverage.general = 0
 			break
 		elseif effective = 1 then
 			--found type combo resistant to full moveset, continue search in case there is a fully immune combo
-			general = 1
+			Coverage.general = 1
 		end
 	end
 	
 	for moveIndex = 1, 4, 1 do
+	--[[NOTE: Mimic, Metronome, Mirror Move, Transform, Sketch, Magic Coat, all entry hazards, and a variety of abilities can also
+	deal with a Shedinja in theory, but they are all wildly inconsistent or require foreknowledge that a Shedinja is on a Trainer's
+	team, so they are not included.]]
 		if pokemon.moves[moveIndex] ~= nil and pokemon.moves[moveIndex].id ~= 0 then
 			if pokemon.moves[moveIndex].category ~= MoveData.Categories.STATUS then
 				if Utils.netEffectiveness(pokemon.moves[moveIndex], pokemon.moves[moveIndex].type, shedType) > 1 then
 				--a single super-effective move is sufficient for Shedinja coverage, end early
-					shedinja = 2
+					Coverage.shedinja = 2
 					break
-				elseif pokemon.moves[moveIndex].id == "101"
+				elseif pokemon.moves[moveIndex].id == "101" then
 				--Night Shade uniquely among fixed-damage moves does affect Shedinja
-					shedinja = 2
+					Coverage.shedinja = 2
 					break
-				elseif pokemon.moves[moveIndex].id == "248" --due to a programming quirk, typeless damage affects Shedinja in gen 3
+				elseif pokemon.moves[moveIndex].id == "248" then
+				--due to a programming quirk, typeless damage affects Shedinja in gen 3
 				--Future Sight
-					shedinja = 2
+					Coverage.shedinja = 2
 					break
-				elseif pokemon.moves[moveIndex].id == "353"
+				elseif pokemon.moves[moveIndex].id == "353" then
 				--Doom Desire
-					shedinja = 2
+					Coverage.shedinja = 2
 					break
 				end
-			elseif
+			elseif shedinja == 0 then
+			--wasteful to do all these extra checks if a move has already been found with partial effectiveness
+				if pokemon.moves[moveIndex].id == "48" then
+				--Supersonic, debatable whether confusion should be used to determine Shedinja coverage, but technically it works
+					Coverage.shedinja = 1
+				elseif pokemon.moves[moveIndex].id == "73" then
+				--Leech Seed
+					Coverage.shedinja = 1
+				elseif pokemon.moves[moveIndex].id == "77" then
+				--Poisonpowder
+					Coverage.shedinja = 1
+				elseif pokemon.moves[moveIndex].id == "92" then
+				--Toxic
+					Coverage.shedinja = 1
+				elseif pokemon.moves[moveIndex].id == "109" then
+				--Confuse Ray
+					Coverage.shedinja = 1
+				elseif pokemon.moves[moveIndex].id == "139" then
+				--Poison Gas
+					Coverage.shedinja = 1
+				elseif pokemon.moves[moveIndex].id == "171" then
+				--Nightmare, if you're going to put in the work to make this happen in ironmon I guess you deserve a yellow light
+					Coverage.shedinja = 1
+				elseif (pokemon.moves[moveIndex].id == "174" and (pokemon.types[1] == PokemonData.Types.GHOST or pokemon.types[2] == PokemonData.Types.GHOST)) then
+				--Curse
+					Coverage.shedinja = 1
+				elseif pokemon.moves[moveIndex].id == "186" then
+				--Sweet Kiss
+					Coverage.shedinja = 1
+				elseif pokemon.moves[moveIndex].id == "201" then
+				--Sandstorm
+					Coverage.shedinja = 1
+				elseif pokemon.moves[moveIndex].id == "207" then
+				--Swagger
+					Coverage.shedinja = 1
+				elseif pokemon.moves[moveIndex].id == "258" then
+				--Hail
+					Coverage.shedinja = 1
+				elseif pokemon.moves[moveIndex].id == "260" then
+				--Flatter
+					Coverage.shedinja = 1
+				elseif pokemon.moves[moveIndex].id == "261" then
+				--Will-O-Wisp
+					Coverage.shedinja = 1
+				elseif pokemon.moves[moveIndex].id == "298" then
+				--Teeter Dance
+					Coverage.shedinja = 1
+				end
+			end
+		end
+	end
 end
-
-
-
-
-
-
-
-        for moveIndex = 1, 4, 1 do
-                if pokemon.moves[moveIndex] ~= nil and pokemon.moves[moveIndex].id ~= 0 then
-                        for k, typeCombo in ipairs(PokemonData.TypeCombos) do
-                                if Utils.netEffectiveness(pokemon.moves[moveIndex], pokemon.moves[moveIndex].type, typeCombo)
-
-
-local movesToCheck = {
-		            MoveData.BlankMove,
-		            MoveData.BlankMove,
-    		        MoveData.BlankMove,
-        		    MoveData.BlankMove,
-	      }
-        
-        local trackedMoves = Tracker.GetMoves(pokemon.pokemonID)
-        
-        
